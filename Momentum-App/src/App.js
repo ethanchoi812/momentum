8f0d62572bf5ffa36c5f33cf9a0f0eb7a01f929d
Sam <climbinsheep@gmail.com>
2017-02-28 14:58:11 +0000

    adding background image and starting settings popup

--- Momentum-App/src/App.js
+++ Momentum-App/src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import Clock from './components/clock.js'
-import Greetings from './components/greetings.js'
-import Settings from './components/settings.js'
-import Weather from './components/weather.js'
-import Todo from './components/todo.js'
+import Clock from './components/clock.js';
+import Greetings from './components/greetings.js';
+import Settings from './components/settings.js';
+import Weather from './components/weather.js';
+import Todo from './components/todo.js';
 
 
 class App extends Component {
+  
+  /*
+  componentWillMount(){
+    var num = 1;
+    document.querySelector('screen').classList.add(`bgi${num}`);
+  }
+  */
+  
   render() {
     return (
-      <div>
+      <div className="screen">
         <Clock />
         <Weather />
         <Greetings />
--- Momentum-App/src/components/settings.js
+++ Momentum-App/src/components/settings.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './settings.css';
+import User from './user';
+import Switch from './switch'
 
 
 
@@ -7,7 +9,28 @@ export default class Settings extends Component {
   render() {
     return (
       <div className="settings">
-        This is the settings box
+        <div className="settingGroups">
+          <div>General</div>
+          <div>ToDo</div>
+          <div>Background</div>
+          <div>Quotes</div>
+        </div>
+        <div className="setGeneral">
+          <User />
+          <div className="onOff">
+            <div className="miniTitle underline">Widgets</div>
+            <Switch item={'Weather'} />
+            <Switch item={"Clock"} />
+            <Switch item={"Focus"} />
+            <Switch item={"Todo"} />
+          </div>
+        </div>
+        <div className="setTodo">
+        </div>
+        <div className="setBackground">
+        </div>
+        <div className="setQuotes">
+        </div>
       </div>
     );
   }
