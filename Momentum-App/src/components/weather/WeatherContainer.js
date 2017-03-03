0befba459065517c2d569cc46fba045e5838d6ac
Kamyl <kamillcybulski@gmail.com>
2017-03-03 21:15:54 +0100

    Added basic skeleton for weather widget.

---
+++ Momentum-App/src/components/weather/WeatherContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './weather.css';
+
+
+
+export default class WeatherContainer extends Component {
+  render() {
+    return (
+      <div className="weatherContainer">
+        <p> This Is the weather Bar!</p>
+      </div>
+    );
+  }
+}
