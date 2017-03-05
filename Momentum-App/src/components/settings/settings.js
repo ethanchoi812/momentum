cf1df599a3d0f8f1f509d86475079befef47980b
Sam Williams <samwcoding@gmail.com>
2017-03-05 10:51:00 +0000

    adding wireframe

--- Momentum-App/src/components/settings/settings.js
+++ Momentum-App/src/components/settings/settings.js
@@ -6,35 +6,37 @@ import Switch from './switch'
 
 
 export default class Settings extends Component {
-  render() {
-    return (
-      <div className="settings">
-        <div className="settingGroups">
-          <div>General</div>
-          <div>ToDo</div>
-          <div>Background</div>
-          <div>Quotes</div>
-        </div>
-        <div className="setGeneral">
-          <User />
-          <div className="onOff">
-            <div className="miniTitle underline">Widgets</div>
-            <Switch item={'Weather'} />
-            <Switch item={"Clock"} />
-            <Switch item={"Focus"} />
-            <Switch item={"Todo"} />
-          </div>
-        </div>
-        <div className="setTodo">
-        </div>
-        <div className="setBackground">
-        </div>
-        <div className="setQuotes">
-        </div>
-        <div id="settingsCog">
-            <img id="cog" src="http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Settings-icon.png" alt="settings" />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div id="settingsCog" onClick={this.toggleSettings()}>
+                    <img id="cog" src="http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Settings-icon.png" alt="settings" />
+                </div>
+                <div className="settings">
+                    <div className="settingGroups">
+                        <div>General</div>
+                        <div>ToDo</div>
+                        <div>Background</div>
+                        <div>Quotes</div>
+                    </div>
+                    <div className="setGeneral">
+                        <User />
+                        <div className="onOff">
+                            <div className="miniTitle"><strong>Widgets</strong></div>
+                            <div className="underline" />
+
+                            <Switch item={'Weather'} />
+                            <Switch item={"Clock"} />
+                            <Switch item={"Focus"} />
+                            <Switch item={"Todo"} />
+                        </div>
+                    </div>
+                    <div className="setTodo"></div>
+                    <div className="setBackground"></div>
+                    <div className="setQuotes">
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
