9d8dd3be61c0fd6d28a63262f38fc0f2d6276c1b
Sam <climbinsheep@gmail.com>
2017-03-06 09:00:19 +0000

    open/close settings

--- Momentum-App/src/components/settings.js
+++ Momentum-App/src/components/settings.js
@@ -1,35 +1,47 @@
 import React, { Component } from 'react';
 import './settings.css';
 import User from './user';
-import Switch from './switch'
+import Switch from './switch';
 
 
 
 export default class Settings extends Component {
+  
+  toggleSettings(){
+    var settingsClass = document.querySelector(".settings").classList;
+    settingsClass.toggle('hideSettings');
+  }
+  
   render() {
     return (
-      <div className="settings">
-        <div className="settingGroups">
-          <div>General</div>
-          <div>ToDo</div>
-          <div>Background</div>
-          <div>Quotes</div>
+      <div>
+        <div id="settingscog" onClick={this.toggleSettings.bind(this)} >
+          <img id="cog" alt="settings" src="http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Settings-icon.png" />
         </div>
-        <div className="setGeneral">
-          <User />
-          <div className="onOff">
-            <div className="miniTitle underline">Widgets</div>
-            <Switch item={'Weather'} />
-            <Switch item={"Clock"} />
-            <Switch item={"Focus"} />
-            <Switch item={"Todo"} />
+        <div className="settings hideSettings">
+          <div className="settingGroups">
+            <div>General</div>
+            <div>ToDo</div>
+            <div>Background</div>
+            <div>Quotes</div>
           </div>
-        </div>
-        <div className="setTodo">
-        </div>
-        <div className="setBackground">
-        </div>
-        <div className="setQuotes">
+          <div className="setGeneral">
+            <User />
+            <div className="onOff">
+              <div className="miniTitle"><strong>Widgets</strong></div>
+              <div className="underline" />
+              <Switch item={'Weather'} />
+              <Switch item={"Clock"} />
+              <Switch item={"Focus"} />
+              <Switch item={"Todo"} />
+            </div>
+          </div>
+          <div className="setTodo">
+          </div>
+          <div className="setBackground">
+          </div>
+          <div className="setQuotes">
+          </div>
         </div>
       </div>
     );
