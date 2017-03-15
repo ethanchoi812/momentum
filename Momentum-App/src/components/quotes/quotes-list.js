0cc3f58f893a10697e86ebb37c12532f36fd7c0b
Ethan Choi <ethanchoi.812@gmail.com>
2017-03-15 20:14:12 +0800

    Separated Quote component into sub-components

---
+++ Momentum-App/src/components/quotes/quotes-list.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import QuoteRow from './quotes-row.js';
+
+class QuoteList extends Component {
+
+	render(){
+		const rows=[];
+		this.props.quoteArray.forEach(function(quote){
+			rows.push(<QuoteRow quote={quote}/>);
+		});
+
+	return(
+		<div className="quoteWidgetRow">
+			<ul className="quoteList">
+	      		{rows}
+      		</ul>
+      	</div>
+		);
+	}
+}
+
+export default QuoteList;
