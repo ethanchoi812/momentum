0242fce56de8c7773932409b085ab3b4d8de35b3
Ethan Choi <ethanchoi.812@gmail.com>
2017-03-22 23:03:00 +0800

    fixed wrong import naming

--- Momentum-App/src/components/quotes/QuotesContainer.js
+++ Momentum-App/src/components/quotes/QuotesContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { initialQuoteArray } from './quotes-array.js';
+import initialQuoteArray from './quotes-array.js';
+import QuoteIcons from './quotes-icons.js';
 
 const mainQuoteArray = JSON.parse(localStorage.getItem('updateQuoteArray')) || initialQuoteArray;
 
@@ -7,19 +8,20 @@ class QuotesContainer extends Component {
 
 	constructor(props) {    
     super(props);
-    this.state = {quoteArr:mainQuoteArray};  
+    this.props.state = {quoteArr:mainQuoteArray};  
 	}
 
 	render(){
-	const quoteArr = this.state.quoteArr;
-	const i = quoteArr.length;
+	const quoteArray = this.props.state.quoteArr;
+	const i = quoteArray.length;
 	const d = new Date();
 	const n = (d.getDate())%i;
 
 	return(
 	<div className="displayQuoteDiv">
-  		<p className="quoteSentence">{quoteArr[n].text}</p>
-  		<p className="quoteAuthor">{quoteArr[n].author}</p>
+  		<p className="quoteSentence">{quoteArray[n].text}</p>
+  		<p className="quoteAuthor">{quoteArray[n].author}</p>
+  		<QuoteIcons />
   	</div>
 
   		);
