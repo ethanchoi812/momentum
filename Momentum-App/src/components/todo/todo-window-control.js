cf90089491442f93ea65fba40c4168194b1a3f9c
ksmai <kasingmai@gmail.com>
2017-04-04 14:33:39 +0800

    Enhance todo minimization control

--- Momentum-App/src/components/todo/todo-window-control.js
+++ Momentum-App/src/components/todo/todo-window-control.js
@@ -4,7 +4,10 @@ export default class TodoWindowControl extends React.Component {
   render() {
     return (
       <div className="todo-window-control">
-        <h3 className="todo-window-title">TODO</h3>
+        <h3
+          className="todo-window-title"
+          onClick={() => this.props.isMin && this.props.toggleMin()}
+        >TODO</h3>
         <div>
           <button className="todo-btn todo-window-btn"
             onClick={this.props.toggleMin}>
